6d75d48108c53b545a5eb377e0b03c517d97899e
rich <adukyerer@gmail.com>
2022-09-17 07:57:39 +0000

    add modules

---
+++ app/models/like.rb
@@ -0,0 +1,8 @@
+class Like < ApplicationRecord
+ belongs_to :user
+ belongs_to :post 
+ 
+ def post_likes_counter=(count)
+    post.update_attribute 'likes_counter', count
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+    has_many :comments
+    has_many :posts
+    has_many :likes
+    scope :most_recent_post, -> {posts.last(3)}
+end
+
+
