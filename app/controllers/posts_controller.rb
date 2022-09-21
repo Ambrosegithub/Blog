70c15b8e0f7332d6756c658189d669dfd404a12a
rich <adukyerer@gmail.com>
2022-09-21 06:59:35 +0000

    add post and users controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,9 @@
+class PostsController < ApplicationController
+   def index
+    @user = User.find(params[:user_id])
+   end
+
+    def show
+        @user = Post.find(params[:user_id])
+    end
+end
