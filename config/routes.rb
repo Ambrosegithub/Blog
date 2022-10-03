475d67589228d04dda0426c3c589fcf55ca3aeff
rich <adukyerer@gmail.com>
2022-10-03 08:30:24 +0000

    Add Devise root to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do  
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root to: "users#show" 
   # Defines the root path route ("/")
   # root "articles#index"
   resources :users, only: [:index, :show] do resources :posts, only: [:index, :show] 
   end
-  resources :posts, only: [:index, :show, :new, :create]
-  resources :comments, only: [:new, :create]
+  resources :posts
+  resources :comments, only: [:new, :create, :destroy]
   resources :likes, only: [:create]
 end
