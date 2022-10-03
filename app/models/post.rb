7426a87cdd6a033a35d7728279123de6763f8493
rich <adukyerer@gmail.com>
2022-10-03 08:27:57 +0000

    Add destroy dependency to models user and post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 250 }
 
-  has_many :likes
-  has_many :comments
+  has_many :likes,dependent: :destroy
+  has_many :comments,dependent: :destroy
   belongs_to :user, foreign_key: 'user_id'
   def update_post_count(count)
     user.update_attribute 'posts_counter', count
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   validates :name, presence: true, length: { maximum: 50 }
-  validates :postCounter, presence: true, numericality: { only_integer: true }
+  validates :post_counter, presence: true, numericality: { only_integer: true }
 
-  has_many :comments
-  has_many :posts, foreign_key: 'user_id'
-  has_many :likes, foreign_key: 'user_id'
+  has_many :comments,dependent: :destroy
+  has_many :posts, foreign_key: 'user_id',dependent: :destroy
+  has_many :likes, foreign_key: 'user_id',dependent: :destroy
   def recent_comments
     comments.last(3)
   end
