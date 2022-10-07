601c3f1ddba3e29cf1d4f29a36e5a2ce06a378a0
rich <adukyerer@gmail.com>
2022-10-07 09:07:23 +0000

    Add confirmable to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,18 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+# :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable,:rememberable, :validatable,:confirmable
+         
   validates :name, presence: true, length: { maximum: 50 }
   validates :post_counter, presence: true, numericality: { only_integer: true }
 
+ 
   has_many :comments,dependent: :destroy
   has_many :posts, foreign_key: 'user_id',dependent: :destroy
   has_many :likes, foreign_key: 'user_id',dependent: :destroy
+
+
   def recent_comments
     comments.last(3)
   end
